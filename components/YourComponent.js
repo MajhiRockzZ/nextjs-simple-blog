a3c2670024eff38be087c2ad0907ed5aa32f711f
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-22 12:48:35 +0530

    📦 NEW: Add Assets, Metadata, CSS

---
+++ components/YourComponent.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const YourComponent = () => {
+  return (
+    <Image
+      src="../public/images/profile.jpg"
+      height={144}
+      width={144}
+      alt="Your Name"
+    />
+  );
+};
